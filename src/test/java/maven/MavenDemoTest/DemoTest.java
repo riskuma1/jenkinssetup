939597c062d5b8e2425fff8103a24ef79670cd69
RishiKumar <RishiKumar@riskuma1.in.ibm.com>
2019-04-19 18:46:27 +0530

    jenkins

---
+++ src/test/java/maven/MavenDemoTest/DemoTest.java
@@ -0,0 +1,42 @@
+package maven.MavenDemoTest;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+  @Test
+  public void Sum() {
+	  System.out.println("Sum");
+	  int a= 10;
+	  int b =20;
+	  Assert.assertEquals(30, a+b);
+  }
+  
+  @Test
+  public void Sub() {
+	  System.out.println("Sub");
+	  int a= 10;
+	  int b =20;
+	  Assert.assertEquals(10, b-a);
+  }
+  
+  @Test
+  public void Mul() {
+	  System.out.println("Mul");
+	  int a= 10;
+	  int b =20;
+	  Assert.assertEquals(200, a*b);
+  }
+  
+  @Test
+  public void Div() {
+	  System.out.println("Div");
+	  int a= 10;
+	  int b =20;
+	  Assert.assertEquals(2, b/a);
+  }
+  
+}
